dd73a1e1ecf7e868db050f41e8638a522d590edd
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-22 10:49:00 -0700

    created reducer function and using redux

--- redux-demo.js
+++ redux-demo.js
@@ -1,5 +1,18 @@
 const redux = require("redux");
 
-const counterReducer = () => {};
+const counterReducer = (state, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
 
-const store = redux.createStore();
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+for (let x = 0; x < 10; x++) {
+  counterSubscriber();
+}
